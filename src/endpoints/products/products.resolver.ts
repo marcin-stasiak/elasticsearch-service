6dcaeb256dbc918f8a1f1192e36138175159470b
Marcin Stasiak <kontakt@marcinstasiak.pl>
2022-02-09 07:33:19 +0100

    rename directory

--- src/endpoints/products/products.resolver.ts
+++ src/endpoints/products/products.resolver.ts
@@ -1,8 +1,8 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 
-import { CreateProductInput } from './dto/create-product.input';
-import { UpdateProductInput } from './dto/update-product.input';
 import { Product } from './entities/product.entity';
+import { CreateProductInput } from './interfaces/create-product.input';
+import { UpdateProductInput } from './interfaces/update-product.input';
 import { ProductsService } from './products.service';
 
 @Resolver(() => Product)
--- src/endpoints/products/products.service.ts
+++ src/endpoints/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
-import { CreateProductInput } from './dto/create-product.input';
-import { UpdateProductInput } from './dto/update-product.input';
+import { CreateProductInput } from './interfaces/create-product.input';
+import { UpdateProductInput } from './interfaces/update-product.input';
 
 @Injectable()
 export class ProductsService {
