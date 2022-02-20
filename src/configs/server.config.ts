4242bf0b6841f4ed4efc9f374797c1ce9f16a333
Marcin Stasiak <kontakt@marcinstasiak.pl>
2022-02-20 12:31:50 +0100

    add public dir

--- src/configs/server.config.ts
+++ src/configs/server.config.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  development: process.env.NODE_ENV !== 'production' || true,
+  development: process.env.NODE_ENV !== 'production',
   port: Number.parseInt(process.env.PORT, 10) || 3000,
   elasticsearch: process.env.ELASTIC_SEARCH_URL || '',
 });
